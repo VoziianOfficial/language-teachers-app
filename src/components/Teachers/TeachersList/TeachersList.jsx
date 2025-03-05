16bd29c0201f8766061fbca3b017848e3e89222f
Yuliia Voziian <vozianulia@gmail.com>
2025-03-05 23:59:13 +0200

    update

--- src/components/Teachers/TeachersList/TeachersList.jsx
+++ src/components/Teachers/TeachersList/TeachersList.jsx
@@ -1,55 +1,10 @@
-import { useEffect, useState } from "react";
-import TeacherItem from "../TeacherItem/TeacherItem";
-import { getAllTeachers } from "../../../firebase/api";
-import NoFindTeacher from "../NoFindTeacher/NoFindTeacher";
 import s from './TeachersList.module.css'
-
-
-const TeachersList = ({ filtered, lvl }) => {
-    const [teachersPerPage, setTeachersPerPage] = useState(4);
-    const [favoritesPerPage, setFavoritesPerPage] = useState(4);
-    const [teachers, setTeachers] = useState(null);
-
-    useEffect(() => {
-        const getTeachersData = async () => {
-            let teachersData = await getAllTeachers(teachersPerPage);
-            setTeachers(teachersData);
-        };
-        getTeachersData();
-    }, [teachersPerPage]);
-
-    const handleShowMore = () => {
-        setTeachersPerPage((prev) => (prev += 4));
-    };
-
-    const favoritesShowMore = () => {
-        setFavoritesPerPage((prev) => (prev += 4));
-    };
-
+const TeachersList = () => {
     return (
-        <div className={s.container}>
-            <div className={s.list}>
-                {filtered
-                    ? filtered?.slice(0, favoritesPerPage)?.map((teach, index) => (
-                        <TeacherItem key={index} teach={teach} lvl={lvl} />
-                    ))
-                    : teachers?.map((teach, index) => (
-                        <TeacherItem key={index} teach={teach} lvl={lvl} />
-                    ))}
-                {filtered?.length === 0 && <NoFindTeacher />}
-            </div>
-            {(teachers?.length < 30 && !filtered) && (
-                <button className={s.showMoreBtn} type="button" onClick={handleShowMore}>
-                    Show more
-                </button>
-            )}
-            {filtered?.length > favoritesPerPage && (
-                <button className={s.showMore} type="button" onClick={favoritesShowMore}>
-                    Show more
-                </button>
-            )}
-        </div>
-    );
-};
+        <div className={s.teachersList}>
 
-export default TeachersList;
+        </div>
+    )
+}
+
+export default TeachersList
