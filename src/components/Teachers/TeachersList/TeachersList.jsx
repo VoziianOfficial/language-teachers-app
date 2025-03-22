5e688b2df693db78350f9ae4797abd8a84076c93
Yuliia Voziian <vozianulia@gmail.com>
2025-03-23 00:33:23 +0200

    add styles for teacher items

--- src/components/Teachers/TeachersList/TeachersList.jsx
+++ src/components/Teachers/TeachersList/TeachersList.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../firebase/firebaseConfig";
 import SearchBar from "../SearchBar/SearchBar";
-import NoFindTeacher from '../NoFindTeacher/NoFindTeacher'
+import NoFindTeacher from '../NoFindTeacher/NoFindTeacher';
+import s from './TeachersList.module.css'
 
 
 const TeachersList = () => {
@@ -45,7 +46,7 @@ const TeachersList = () => {
     const indexOfFirstTeacher = indexOfLastTeacher - teachersPerPage;
     const currentTeachers = filteredTeachers.slice(indexOfFirstTeacher, indexOfLastTeacher);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    // const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <div>
@@ -53,18 +54,37 @@ const TeachersList = () => {
             {currentTeachers.length === 0 ? (
                 <NoFindTeacher />
             ) : (
-                <ul>
+                <ul className={s.teacherList}>
                     {currentTeachers.map((teacher) => (
-                        <li key={teacher.id}>
-                            <div style={{ display: "flex", alignItems: "center" }}>
-                                <img src={teacher.avatar_url} alt={teacher.name} style={{ width: 60, height: 60, borderRadius: "50%", marginRight: 10 }} />
-                                <h3>{teacher.name} {teacher.surname}</h3>
+                        <li className={s.teacherItem} key={teacher.id}>
+                            <div className={s.wrapContainer}>
+                                <div className={s.nameWrap} style={{ display: "flex", alignItems: "center" }}>
+                                    <img src={teacher.avatar_url} alt={teacher.name} style={{ width: 60, height: 60, borderRadius: "50%", marginRight: 10 }} />
+                                    <div className={s.nameContent}>
+                                        <p className={s.Languages}>Languages</p>
+                                        <h3>{teacher.name} {teacher.surname}</h3>
+                                    </div>
+                                </div>
+                                <div className={s.infoWrap}>
+                                    <p className={s.textWithIcon}>
+                                        <img src="/src/assets/icons/book-open.svg" alt="Online lessons icon" width="16" height="16" />
+                                        Lessons online
+                                    </p>
+                                    <span className={s.span}>|</span>
+                                    <p className={s.textStyles}><strong className={s.textStyles}>Lessons Done:</strong> {teacher.lessons_done}</p>
+                                    <span className={s.span}>|</span>
+                                    <p className={s.textStyles}><img className={s.imgStar} src="/src/assets/icons/Star 2.svg" alt="" /><strong className={s.textStyles}>Rating:</strong> {teacher.rating}</p>
+                                    <span className={s.span}>|</span>
+                                    <p className={s.textStylesPrise}>
+                                        <strong className={s.textStyles}>Price / 1 hour:</strong>
+                                        <span className={s.priceValue}> ${teacher.price_per_hour}</span>
+                                    </p>
+
+                                </div>
                             </div>
-                            <p><strong>Price / 1 hour:</strong> ${teacher.price_per_hour}</p>
-                            <p><strong>Rating:</strong> {teacher.rating} stars</p>
+
                             <p><strong>Speaks:</strong> {teacher.languages?.join(", ")}</p>
                             <p><strong>Levels:</strong> {teacher.levels?.join(", ")}</p>
-                            <p><strong>Lessons Done:</strong> {teacher.lessons_done}</p>
                             <p><strong>Experience:</strong> {teacher.experience}</p>
                             <p><strong>Lesson Info:</strong> {teacher.lesson_info}</p>
                             <p><strong>Conditions:</strong> {teacher.conditions?.join(", ")}</p>
@@ -82,14 +102,15 @@ const TeachersList = () => {
             )}
 
             {/* Пагинация */}
-            <div>
+            {/* <div>
                 {[...Array(Math.ceil(filteredTeachers.length / teachersPerPage))].map((_, index) => (
                     <button key={index} onClick={() => paginate(index + 1)}>
                         {index + 1}
                     </button>
                 ))}
-            </div>
+            </div> */}
         </div>
+
     );
 };
 
